19725395b21176b209542c4bb9b85090e050b604
vilasbtw <kaiquevilas@icloud.com>
2025-01-15 10:53:01 -0300

    Adding class: 09-ContentNegotiation (Implementing with Query Param/String)

---
+++ 09-ContentNegotiation/src/main/java/com/kaique/spring/data/vo/v1/PersonVO.java
@@ -0,0 +1,72 @@
+package com.kaique.spring.data.vo.v1;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PersonVO implements Serializable {
+
+    private static final Long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String gender;
+
+    public PersonVO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof PersonVO person)) return false;
+        return Objects.equals(id, person.id) && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(address, person.address) && Objects.equals(gender, person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, address, gender);
+    }
+}
