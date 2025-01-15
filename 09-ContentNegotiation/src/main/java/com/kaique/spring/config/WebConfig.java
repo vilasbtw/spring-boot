aa3e53df6b6d64e901fd1afe468f4a22c4c8619e
vilasbtw <kaiquevilas@icloud.com>
2025-01-15 11:07:25 -0300

    Implementing Header Param Content Negotiation

--- 09-ContentNegotiation/src/main/java/com/kaique/spring/config/WebConfig.java
+++ 09-ContentNegotiation/src/main/java/com/kaique/spring/config/WebConfig.java
@@ -8,13 +8,30 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    // By EXTENSION: localhost:8080/api/person/v1.xml (DEPRECATED)
+    // By QUERY PARAM/SPRING: localhost:8080/api/person/v1?mediaType=xml
+    // By HEADER PARAM: localhost:8080/api/person/v1
+
+    /*
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
                   configurer.favorParameter(true)
-                  .parameterName("mediaType").ignoreAcceptHeader(true)
+                  .parameterName("mediaType")
+                  .ignoreAcceptHeader(true)
                   .useRegisteredExtensionsOnly(false)
                   .defaultContentType(MediaType.APPLICATION_JSON)
                   .mediaType("json", MediaType.APPLICATION_JSON)
                   .mediaType("xml", MediaType.APPLICATION_XML);
+    }
+    */
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.favorParameter(false)
+                .ignoreAcceptHeader(false)
+                .useRegisteredExtensionsOnly(false)
+                .defaultContentType(MediaType.APPLICATION_JSON)
+                .mediaType("json", MediaType.APPLICATION_JSON)
+                .mediaType("xml", MediaType.APPLICATION_XML);
     }
 }
