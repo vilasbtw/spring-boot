06531570339367eb3bdca313f36d16b729c80c48
vilasbtw <kaiquevilas@icloud.com>
2025-01-30 13:38:58 -0300

    Implementing Content Negotiation

--- 11-Swagger/src/main/java/com/kaique/spring/controllers/BooksController.java
+++ 11-Swagger/src/main/java/com/kaique/spring/controllers/BooksController.java
@@ -4,14 +4,14 @@ import com.kaique.spring.data.vo.v1.BooksVO;
 import com.kaique.spring.services.BooksService;
 import com.kaique.spring.util.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 /* TO-DO:
-    1. Implement content negotiation;
-    2. Implement support to HATEOAS;
-    3. Implement support to Swagger OpenAPI;
+    1. Implement support to HATEOAS;
+    2. Implement support to Swagger OpenAPI;
 */
 
 @RestController
@@ -21,28 +21,31 @@ public class BooksController {
     @Autowired
     BooksService service;
 
-    @PostMapping(produces = {MediaType.APPLICATION_JSON}, consumes = {MediaType.APPLICATION_JSON})
+    @PostMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public BooksVO create(@RequestBody BooksVO bookVO) {
         return service.create(bookVO);
     }
 
-    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON})
+    @GetMapping(value = "/{id}", produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public BooksVO findById(@PathVariable(value = "id") Long id) {
         return service.findById(id);
     }
 
-    @GetMapping(produces = {MediaType.APPLICATION_JSON})
+    @GetMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public List<BooksVO> findAll() {
         return service.findAll();
     }
 
-    @PutMapping(produces = {MediaType.APPLICATION_JSON}, consumes = {MediaType.APPLICATION_JSON})
+    @PutMapping(produces = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML},
+            consumes = {MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML, MediaType.APPLICATION_YML})
     public BooksVO update(@RequestBody BooksVO bookVO) {
         return service.update(bookVO);
     }
 
     @DeleteMapping(value = "/{id}")
-    public void delete(@PathVariable(name = "id") Long id) {
+    public ResponseEntity<?> delete(@PathVariable(name = "id") Long id) {
         service.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
